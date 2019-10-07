b8fa596aceac25ccb2d08704f8fdc7a605dbbf93
wangao <1366463855@qq.com>
2019-10-07 14:47:58 +0800

    init project

---
+++ frontend/wxapp/src/pages/activity/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <view style="padding:15px;">
+      <i-card :title="item.title" :extra="item.extra" :thumb="item.thumb">
+        <view slot="content">{{item.content}}</view>
+        <view slot="footer">{{item.footer}}</view>
+      </i-card>
+    </view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      item: {}
+    }
+  },
+  components: {},
+  onLoad (options) {
+    console.log('hello')
+    this.item = {
+      'title': '五道口支教',
+      'extra': '队长：王澳',
+      'thumb': 'https://avatars3.githubusercontent.com/u/43805318?s=460&v=4',
+      'description': '我们是五道口支教团队',
+      'content': '我们是五道口团队，啦啦啦啦',
+      'footer': '10月5日 五道口',
+      'time': '10月5日',
+      'location': '五道口',
+      'id': '123',
+      'leader': '王澳'
+    }
+  },
+  methods: {},
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/wxapp/src/pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <i-panel title="申请创建">
+      <view style="padding:15px;">
+        <i-card @click='bindClick(item)' v-for="(item,index) in createdItems" :key=index :title="item.title" :extra="item.extra" :thumb="item.thumb">
+          <view slot="content">{{item.description}}</view>
+          <view slot="footer">{{item.footer}}</view>
+        </i-card>
+      </view>
+    </i-panel>
+    <i-panel title="申请加入">
+      <view style="padding:15px;">
+        <i-card @click='bindClick(item)' v-for="(item,index) in applyedItems" :key=index :title="item.title" :extra="item.extra" :thumb="item.thumb">
+          <view slot="content">{{item.description}}</view>
+          <view slot="footer">{{item.footer}}</view>
+        </i-card>
+      </view>
+    </i-panel>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      createdItems: [
+        {
+          'title': '五道口支教',
+          'extra': '队长：王澳',
+          'thumb': 'https://avatars3.githubusercontent.com/u/43805318?s=460&v=4',
+          'description': '我们是五道口支教团队',
+          'footer': '10月5日 五道口',
+          'time': '10月5日',
+          'location': '五道口',
+          'id': '123'
+        }
+      ],
+      applyedItems: [
+        {
+          'title': '五道口支教',
+          'extra': '队长：王澳',
+          'thumb': 'https://avatars3.githubusercontent.com/u/43805318?s=460&v=4',
+          'description': '我们是五道口支教团队',
+          'footer': '10月5日 五道口',
+          'time': '10月5日',
+          'location': '五道口',
+          'id': '123'
+        }
+      ]
+    }
+  },
+
+  components: {},
+
+  methods: {
+    'bindClick': function (item) {
+      wx.navigateTo({ url: '/pages/activity/main?id=' + item.id })
+    }
+  },
+  onLoad (options) {
+    wx.login({
+      success: res => { console.log(res) },
+      fail: () => { console.log('error') },
+      complete: () => {}
+    })
+  },
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
