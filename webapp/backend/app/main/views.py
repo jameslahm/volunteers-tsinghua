84699b38bd673a50c955134d49885afee0c64333
wangao <1366463855@qq.com>
2019-11-30 19:28:28 +0800

    update myactivity

--- webapp/backend/app/main/views.py
+++ webapp/backend/app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,request,current_app
 from . import main
 from flask_login import login_required,current_user
 from flask import redirect,url_for
@@ -26,9 +26,14 @@ def profile():
 @main.route('/myactivity', methods=['GET'])
 @login_required
 def myactivity():
-    activities=Activity.query.filter_by(team=current_user).all()
+    # page=request.args.get('page',1,type=int)
+    page=1
+    pagination=Activity.query.filter_by(team=current_user).order_by(Activity.starttime.desc()).paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],error_out=False
+    )
+    activities=pagination.items
     return render_template(
-        'myactivity.html',activities=activities
+        'myactivity.html',activities=activities,pagination=pagination
     )
 
 @main.route('/createactivity', methods=['GET', 'POST'])
