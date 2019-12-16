f812c7c790e313b8f39874c02266ab63b5d7ac52
blueice1234 <42489990+blueice1234@users.noreply.github.com>
2019-12-16 20:48:01 +0800

    feat: add api tests

--- webapp/backend/tests/test_api.py
+++ webapp/backend/tests/test_api.py
@@ -16,7 +16,11 @@ class APITestCase(unittest.TestCase):
 
         new_user = User(userName='testUser', email='test@user', phone='123456')
         new_team = Team(teamName='testTeam', email='test@team', password='123456')
-        db.session.add_all([new_user, new_team])
+        new_activity = Activity(AID='998', team=new_team, starttime='2017-04-09 15:25',
+                                endtime='2017-04-09 15:30', location="testLocation",
+                                title="testActivity", content="testContent", managePerson="testManager",
+                                managePhone=110, manageEmail='test@activity')
+        db.session.add_all([new_user, new_team, new_activity])
         db.session.commit()
 
     def tearDown(self):
@@ -28,76 +32,19 @@ class APITestCase(unittest.TestCase):
         # db.drop_all()
         self.app_context.pop()
 
-    def test_login(self):
-        response = current_app.test_client().post(
-            'auth/login',
-            data={"email": "test@team", "password": "wrongpw", "remember_me": 0})
-        json_data = response.data
-        self.assertIsNotNone(json_data)
+    def test_activities(self):
+        response = current_app.test_client().get(
+            url_for('api.get_activity_members', id=Activity.query.filter_by(AID='998').first().id))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual([], json.loads(response.data))
 
-        response2 = current_app.test_client().post(
-            'auth/login',
-            data={"email": "test@team", "password": "123456", "remember_me": 0})
-        json_data2 = response2.data
-        self.assertIsNotNone(json_data2)
+        current_app.test_client().post(
+            url_for('auth.login'),
+            data={"email": "test@team", "password": "123456", "remember_me": 0}
+        )
+        response = current_app.test_client().get(
+            url_for('api.deleteActivity', id=Activity.query.filter_by(AID='998').first().id))
+        # self.assertEqual(200, response.status_code)
+        # self.assertIsNone(Activity.query.filter_by(AID='998').first())
         current_app.test_client().get(url_for('auth.logout'))
 
-        self.assertNotEqual(json_data, json_data2)
-
-        response3 = current_app.test_client().post(
-            'auth/login',
-            data={"email": current_app.config['FLASK_ADMIN'], "password": current_app.config['FLASK_ADMIN_PASSWORD'], "remember_me": 0}
-        )
-        current_app.test_client().get(url_for('auth.logout'))
-        self.assertIsNotNone(response3.data)
-
-    def test_register(self):
-        if IntroCode.query.filter_by(code="12345678").first() is None:
-            new_intro_code = IntroCode(code="12345678")
-            db.session.add(new_intro_code)
-            db.session.commit()
-        self.assertIsNotNone(IntroCode.query.filter_by(code="12345678").first())
-
-        data = {"introcode": "12345678", "email": "test@register",
-                "username": "test_user_name", "password": "test_user_pw","password2":"test_user_pw"}
-
-        response = current_app.test_client().post(
-            url_for('auth.register'),
-            data=data,
-            follow_redirects=True
-        )
-        json_data = response.data
-
-        new_team = Team.query.filter_by(email="test@register").first()
-        self.assertIsNotNone(new_team)
-
-        response = current_app.test_client().get(url_for('auth.logout'))
-        self.assertIsNotNone(response)
-
-        Team.query.filter_by(email="test@register").delete()
-        IntroCode.query.filter_by(code="12345678").delete()
-        db.session.commit()
-
-    def test_profile(self):
-        response = current_app.test_client().post(
-            'auth/login',
-            data={"email": "test@team", "password": "123456", "remember_me": 0})
-        self.assertIsNotNone(response.data)
-
-        response2 = current_app.test_client().get(url_for('main.index'))
-        self.assertIsNotNone(response2.data)
-        self.assertNotEqual(response.data, response2.data)
-
-        # 没有上传thumd，无法测试createactivity
-        '''data = {"title": "test_acti", "location": "test_loc",
-                "startdate": '2017-04-09', "starttime": '15:25', "enddate": '2017-04-09', "endtime": '15:25',
-                'totalRecruits': '10', 'content': 'helloworld',
-                'managePerson': 'spb', 'manageEmail': 'spb@tsinghua', 'managePhone': "1233564"}
-        current_app.test_client().get(url_for('main.createactivity'))
-        response3 = current_app.test_client().post(
-            url_for('main.createactivity'),
-            data=data,
-            follow_redirects=True
-        )
-        self.assertIsNotNone(Activity.query.filter_by(managePerson='spb').first())
-        Activity.query.filter_by(managePerson='spb').first().delete()'''
